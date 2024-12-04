a9ac797226f34cc4d85635854a710959343244d7
Carson M-L <45301215+CarsonML@users.noreply.github.com>
2024-12-04 00:40:02 -0800

    change to get gt dino features

--- scene/cameras.py
+++ scene/cameras.py
@@ -40,6 +40,9 @@ class Camera(nn.Module):
         self.image_width = self.original_image.shape[2]
         self.image_height = self.original_image.shape[1]
 
+        self.cropped_image_width = (self.original_image.shape[2] // 14) * 14
+        self.cropped_image_height = (self.original_image.shape[1] // 14) * 14
+
         if gt_alpha_mask is not None:
             self.original_image *= gt_alpha_mask.to(self.data_device)
         else:
@@ -57,39 +60,11 @@ class Camera(nn.Module):
         self.camera_center = self.world_view_transform.inverse()[3, :3]
     def get_language_feature(self, language_feature_dir, feature_level):
         language_feature_name = os.path.join(language_feature_dir, self.image_name)
-        seg_map = torch.from_numpy(np.load(language_feature_name + '_s.npy'))
         feature_map = torch.from_numpy(np.load(language_feature_name + '_f.npy'))
         
-        # elif str(language_feature_name).split('.')[-1] == 'pkl':
-        #     with open(language_feature_name, 'rb') as f:
-        #         data = pickle.load(f)
-        #     seg_map = data['seg_maps']
-        #     feature_tensor = data['feature']
-        # print(seg_map.shape, feature_tensor.shape)torch.Size([4, 832, 1264]) torch.Size([391, 512])
-        # feature_map = torch.zeros(512, self.image_height, self.image_width)
-        y, x = torch.meshgrid(torch.arange(0, self.image_height), torch.arange(0, self.image_width))
-        x = x.reshape(-1, 1)
-        y = y.reshape(-1, 1)
-        seg = seg_map[:, y, x].squeeze(-1).long()
-        mask = seg != -1
-        if feature_level == 0: # default
-            point_feature1 = feature_map[seg[0:1]].squeeze(0)
-            mask = mask[0:1].reshape(1, self.image_height, self.image_width)
-        elif feature_level == 1: # s
-            point_feature1 = feature_map[seg[1:2]].squeeze(0)
-            mask = mask[1:2].reshape(1, self.image_height, self.image_width)
-        elif feature_level == 2: # m
-            point_feature1 = feature_map[seg[2:3]].squeeze(0)
-            mask = mask[2:3].reshape(1, self.image_height, self.image_width)
-        elif feature_level == 3: # l
-            point_feature1 = feature_map[seg[3:4]].squeeze(0)
-            mask = mask[3:4].reshape(1, self.image_height, self.image_width)
-        else:
-            raise ValueError("feature_level=", feature_level)
-        # point_feature = torch.cat((point_feature2, point_feature3, point_feature4), dim=-1).to('cuda')
-        point_feature = point_feature1.reshape(self.image_height, self.image_width, -1).permute(2, 0, 1)
+        point_feature = feature_map.reshape(-1, self.cropped_image_height, self.cropped_image_width)
        
-        return point_feature.cuda(), mask.cuda()
+        return point_feature.cuda()
 
 class MiniCam:
     def __init__(self, width, height, fovy, fovx, znear, zfar, world_view_transform, full_proj_transform):
